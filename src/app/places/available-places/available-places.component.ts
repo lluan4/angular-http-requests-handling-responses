7be988bcf4d5d89a38643e210b7d0f557b430b5e
LuanLima <lluan.lima94@gmail.com>
2025-08-27 14:57:29 -0300

    Refactor available places component to handle loading, error, and empty states; integrate HTTP client for data fetching

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,18 @@
-import { Component, signal } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, Subscription, throwError } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +21,39 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
   places = signal<Place[] | undefined>(undefined);
+  error = signal<string>('');
+  status = signal<'loading' | 'success' | 'empty' | 'error'>('loading');
+
+  ngOnInit() {
+    this.status.set('loading');
+    const subscription$: Subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places', {
+        observe: 'response',
+      })
+      .pipe(
+        map((response) => response.body?.places),
+        catchError(({ error }) => {
+          return throwError(() => new Error(error?.message));
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+          this.status.set(places?.length ? 'success' : 'empty');
+        },
+        error: (error: Error) => {
+          this.status.set('error');
+          this.error.set(error.message);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription$.unsubscribe();
+    });
+  }
 }
